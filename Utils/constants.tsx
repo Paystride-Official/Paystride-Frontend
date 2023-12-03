6e9dbd0a7ec6af710922e51a7005e0360e13cf58
MadukaJustice <jonasjustice98@gmail.com>
2023-12-03 14:13:23 +0100

    implemented transaction page table

--- Utils/constants.tsx
+++ Utils/constants.tsx
@@ -64,6 +64,73 @@ export const dashboardCol = [
     label: "Amount",
   },
 ];
+
+export const transactionRow = [
+  {
+    key: "1",
+    date: "Oct 12, 2023",
+    transactionId: "6748928879999003",
+    accountNumber: "0001100110",
+    ppointNumber: "08123893451",
+    amount: "N 20,000",
+  },
+  {
+    key: "1",
+    date: "Oct 12, 2023",
+    transactionId: "6748928879999003",
+    accountNumber: "0001100110",
+    ppointNumber: "08123893451",
+    amount: "N 20,000",
+  },
+  {
+    key: "1",
+    date: "Oct 12, 2023",
+    transactionId: "6748928879999003",
+    accountNumber: "0001100110",
+    ppointNumber: "08123893451",
+    amount: "N 20,000",
+  },
+  {
+    key: "1",
+    date: "Oct 12, 2023",
+    transactionId: "6748928879999003",
+    accountNumber: "0001100110",
+    ppointNumber: "08123893451",
+    amount: "N 20,000",
+  },
+  {
+    key: "1",
+    date: "Oct 12, 2023",
+    transactionId: "6748928879999003",
+    accountNumber: "0001100110",
+    ppointNumber: "08123893451",
+    amount: "N 20,000",
+  },
+];
+
+export const transactionCol = [
+  {
+    key: "date",
+    label: "Date & Time",
+  },
+  {
+    key: "transactionId",
+    label: "Transaction Id",
+  },
+  {
+    key: "accountNumber",
+    label: "Account Number",
+  },
+  {
+    key: "ppointNumber",
+    label: "P.Point Number",
+  },
+  {
+    key: "amount",
+    label: "Amount",
+  },
+];
+
 export const payPointRow = [
   {
     key: "1",
--- app/(analytics)/transaction/page.tsx
+++ app/(analytics)/transaction/page.tsx
@@ -1,9 +1,52 @@
-import React from "react";
+"use client";
+import { transactionCol, transactionRow } from "@/Utils/constants";
+import Controllers from "@/components/Controllers/Controllers";
+import { TableComponent } from "@/components/Table/Table";
+import { useState } from "react";
 
 type Props = {};
 
-const transaction = (props: Props) => {
-  return <div>transaction</div>;
+const Transaction = (props: Props) => {
+  const [search, setSearch] = useState<string>("");
+  const [filters, setFilters] = useState<string>("");
+
+  return (
+    <div>
+      <div
+        className="
+          text-gray-600
+            font-inter
+            text-3xl
+            leading-5
+            font-bold
+            mt-12
+            "
+      >
+        <p>Transactions</p>
+      </div>
+
+      <div
+        className="    
+           bg-white  
+            rounded-[6.198px]
+            border-[2.066px]
+            border-solid
+            border-[#eceef6]
+            mt-8
+            px-4
+            "
+      >
+        <Controllers
+          search={search}
+          setSearch={setSearch}
+          filters={filters}
+          setFilters={setFilters}
+        />
+
+        <TableComponent rows={transactionRow} columns={transactionCol} />
+      </div>
+    </div>
+  );
 };
 
-export default transaction;
+export default Transaction;
