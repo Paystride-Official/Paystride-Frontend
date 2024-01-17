96d419892e8a3503b9a58d53d617239a4d18f80f
Ifeoma <ifeoma.ofodile.231807@unn.edu.ng>
2024-01-17 20:11:14 +0100

    Marketing folder adjustments

---
+++ app/(marketing)/_components/Footer/Footer.tsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import React from "react";
+import PaystrideWhite from "@/app/(marketing)/assets/image 11.svg";
+import Facebook from "@/app/(marketing)/assets/Facebook.svg";
+import Instagram from "@/app/(marketing)/assets/Instagram.svg";
+import Twitter from "@/app/(marketing)/assets/Twitter.svg";
+import LinkedIn from "@/app/(marketing)/assets/LinkedIn.svg";
+import Link from "next/link";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+    return (
+        <div className="bg-[#091F8E] w-[100%] flex justify-center py-10 md:py-16">
+            <div className="w-[90%] text-white">
+                <div className="flex flex-col md:flex-row justify-between">
+                    <div className="flex w-full md:w-[60%] lg:w-[50%] flex-col justify-between items-center h-[8rem] md:h-[15rem] mb-5 md:mb-0">
+                        <h1 className="text-2xl md:text-3xl lg:text-[40px] xl:text-5xl capitalize font-bold text-center leading-7 md:leading-10">
+                            Growth your business with Paystride solution now!
+                        </h1>
+
+                        <button className="text-black capitalize bg-white py-[6px] px-3 md:py-2 md:px-4 text-[12px] min-[360px]:text-sm sm:text-base xl:text-xl">
+                            try paystride for free
+                        </button>
+                    </div>
+                    <div className="w-full md:w-[40%] lg:w-[34%] flex flex-col justify-between items-start h-[12rem] md:h-[15rem]">
+                        <Image src={PaystrideWhite} alt="LogoNameImage" />
+                        <div className="text-xs md:text-sm">
+                            <h3 className="font-semibold">Address:</h3>
+                            <p>Level 1, 12 Sample St, Sydney NSW 2000</p>
+                        </div>
+                        <div className="text-xs md:text-sm">
+                            <h3 className="font-semibold">Contact:</h3>
+                            <p>1800 123 4567</p>
+                            <p>info@relume.io</p>
+                        </div>
+                        <div className="flex gap-4">
+                            <Link href="">
+                                <Image src={Facebook} alt="FacebookIcon" />
+                            </Link>
+                            <Link href="">
+                                <Image src={Instagram} alt="InstagramIcon" />
+                            </Link>
+                            <Link href="">
+                                <Image src={Twitter} alt="TwitterIcon" />
+                            </Link>
+                            <Link href="">
+                                <Image src={LinkedIn} alt="LinkedInIcon" />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <hr className="mt-10" />
+                <div className="text-xs md:text-sm capitalize flex flex-col md:flex-row justify-between">
+                    <div className="md:gap-4 flex pt-5 justify-evenly">
+                        <Link href="" className="underline">
+                            <p>privacy policy</p>
+                        </Link>
+                        <Link href="" className="underline">
+                            <p>terms of service</p>
+                        </Link>
+                        <Link href="" className="underline">
+                            <p>cookie settings</p>
+                        </Link>
+                    </div>
+                    <div className="pt-5 text-center md:text-end flex flex-col min-[1000px]:flex-row">
+                        <span>&copy; 2023 paystride. all rights reserved.</span>
+                        <span className="pl-[6px]">
+                            powered by topstride innovation
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- app/(marketing)/_components/Navbar/Navbar.tsx
+++ app/(marketing)/_components/Navbar/Navbar.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import React, { useState } from "react";
 import Link from "next/link";
 import Menu from "@/app/(marketing)/assets/Menu.svg";
-import Paystride from "@/app/assets/Paystride.svg";
-// import PaystrideWhite from "@/app/(marketing)/assets/image 11.svg";
+import Paystride from "@/app/(marketing)/assets/image 10.svg";
+import PaystrideWhite from "@/app/(marketing)/assets/image 11.svg";
 import Close from "@/app/(marketing)/assets/Close.svg";
 import Cmark from "@/app/(marketing)/assets/Cmark.svg";
 import TopMark from "@/app/(marketing)/assets/TopMark.svg";
@@ -14,123 +14,142 @@ import BottomMark from "@/app/(marketing)/assets/BottomMark.svg";
 type Props = {};
 
 const Navbar = (props: Props) => {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  return (
-    <div className="h-fit w-full sticky top-0 bg-white border-b-[#21232a] border-b border-solid z-[1000]">
-      <div className="flex items-center justify-between p-2 md:py-3 w-[90%] mx-auto">
-        <div className="flex gap-2 items-center">
-          <Link href="/">
-            <Image
-              src={Paystride}
-              alt="Paystride"
-              className="h-10 md:h-12 w-[100%] flex items-center"
-            />
-          </Link>
-          <h1 className="text-lg font-bold text-[#6A8F9F] ">Paystride</h1>
-        </div>
-        <div>
-          <ul className="hidden md:flex items-center gap-5">
-            <li>
-              <Link href="/features">Features</Link>
-            </li>
-            <li>
-              <Link href="/pricing">Pricing</Link>
-            </li>
-            <li>
-              <Link href="/login">
-                <button
-                  type="button"
-                  className="border border-solid border-[#21232a] py-1 px-3 md:py-2 md:px-4 text-[12px] md:text-base xl:text-xl capitalize"
-                >
-                  Login
-                </button>
-              </Link>
-            </li>
-            <li>
-              <Link href="/sign-up">
-                <button
-                  type="button"
-                  className="text-white capitalize bg-[#091F8E] py-1 px-3 md:py-2 md:px-4 text-[12px] md:text-base xl:text-xl"
-                >
-                  try paystride for free
-                </button>
-              </Link>
-            </li>
-          </ul>
-          <div
-            className="md:hidden cursor-pointer"
-            onClick={() => setOpen(!open)}
-          >
-            <Image src={Menu} alt="Menu" />
-          </div>
+    return (
+        <div className="h-fit w-full sticky top-0 bg-white border-b-[#21232a] border-b border-solid z-[1000]">
+            <div className="flex items-center justify-between px-2 py-4 md:py-5 w-[90%] mx-auto">
+                <div className="flex gap-2 items-center">
+                    <Link href="/">
+                        <Image
+                            src={Paystride}
+                            alt="Paystride"
+                            className="h-10 md:h-12 w-[100%] flex items-center"
+                        />
+                    </Link>
+                </div>
+                <div>
+                    <ul className="hidden md:flex items-center gap-5">
+                        <li>
+                            <Link href="/features">Features</Link>
+                        </li>
+                        <li>
+                            <Link href="/pricing">Pricing</Link>
+                        </li>
+                        <li>
+                            <Link href="/login">
+                                <button
+                                    type="button"
+                                    className="border border-solid border-[#21232a] py-1 px-3 md:py-2 md:px-4 text-[12px] md:text-base xl:text-xl capitalize"
+                                >
+                                    Login
+                                </button>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/free-trial">
+                                <button
+                                    type="button"
+                                    className="text-white capitalize bg-[#091F8E] py-2 px-4 text-base xl:text-xl"
+                                >
+                                    try paystride for free
+                                </button>
+                            </Link>
+                        </li>
+                    </ul>
+                    <div
+                        className="md:hidden cursor-pointer"
+                        onClick={() => setOpen(!open)}
+                    >
+                        <Image src={Menu} alt="Menu" />
+                    </div>
 
-          {/* mobile view */}
-          <div
-            className={`absolute md:hidden bg-[#051355] text-white w-full h-[100vh] top-0 left-0 duration-0 ${
-              open ? "left-0 " : "left-[-100%]"
-            }`}
-          >
-            <div className="px-[5%] border-b-white border-b border-solid relative z-20">
-              <div className="flex justify-between p-2">
-                <Link href="/" onClick={() => setOpen(!open)}>
-                  <Image src={Cmark} alt="Paystride" className="h-10 w-full" />
-                </Link>
-                <Image
-                  onClick={() => setOpen(!open)}
-                  src={Close}
-                  alt="Close"
-                  className="cursor-pointer"
-                />
-              </div>
+                    {/* mobile view */}
+                    <div
+                        className={`absolute md:hidden bg-[#051355] text-white w-full h-[100vh] top-0 left-0 duration-0 ${
+                            open ? "left-0 " : "left-[-200%]"
+                        }`}
+                    >
+                        <div className="px-[5%] border-b-white border-b border-solid relative z-20">
+                            <div className="flex justify-between items-center px-2 py-4">
+                                <Link href="/" onClick={() => setOpen(!open)}>
+                                    <Image
+                                        src={PaystrideWhite}
+                                        alt="Paystride"
+                                        className="h-10 w-full"
+                                    />
+                                </Link>
+                                <Image
+                                    onClick={() => setOpen(!open)}
+                                    src={Close}
+                                    alt="Close"
+                                    className="cursor-pointer w-[30px] h-[30px]"
+                                />
+                            </div>
+                        </div>
+                        <ul className="relative text-center py-7 px-[5%] z-20">
+                            <li className="hover:font-bold hover:text-[#d0d0df] w-full py-3">
+                                <Link
+                                    href="/features"
+                                    onClick={() => setOpen(!open)}
+                                >
+                                    Features
+                                </Link>
+                            </li>
+                            <li className="hover:font-bold hover:text-[#d0d0df] w-full py-3">
+                                <Link
+                                    href="/pricing"
+                                    onClick={() => setOpen(!open)}
+                                >
+                                    Pricing
+                                </Link>
+                            </li>
+                            <li className="hover:font-bold hover:text-[#d0d0df] w-full py-3">
+                                <Link
+                                    href="/login"
+                                    onClick={() => setOpen(!open)}
+                                >
+                                    <button className="border-none py-1 px-3 md:py-2 md:px-4 text-base xl:text-lg capitalize">
+                                        Login
+                                    </button>
+                                </Link>
+                            </li>
+                            <li className="hover:font-bold w-full py-5">
+                                <Link
+                                    href="/free-trial"
+                                    onClick={() => setOpen(!open)}
+                                >
+                                    <button className="text-black capitalize bg-white py-[6px] px-3 text-base">
+                                        try paystride for free
+                                    </button>
+                                </Link>
+                            </li>
+                        </ul>
+                        <Image
+                            src={Cmark}
+                            alt="Vector"
+                            className="absolute top-0 left-0"
+                        />
+                        <Image
+                            src={TopMark}
+                            alt="Vector"
+                            className="absolute top-20 left-[55%] z-10"
+                        />
+                        <Image
+                            src={Wavy}
+                            alt="Vector"
+                            className="absolute top-10 left-[10%] h-full w-[90%]"
+                        />
+                        <Image
+                            src={BottomMark}
+                            alt="Vector"
+                            className="absolute bottom-10 left-0"
+                        />
+                    </div>
+                </div>
             </div>
-            <ul className="relative text-center py-7 px-[5%] z-20">
-              <li className="hover:font-bold hover:text-[#d0d0df] w-full py-3">
-                <Link href="/features" onClick={() => setOpen(!open)}>
-                  Features
-                </Link>
-              </li>
-              <li className="hover:font-bold hover:text-[#d0d0df] w-full py-3">
-                <Link href="/pricing" onClick={() => setOpen(!open)}>
-                  Pricing
-                </Link>
-              </li>
-              <li className="hover:font-bold hover:text-[#d0d0df] w-full py-3">
-                <Link href="/login" onClick={() => setOpen(!open)}>
-                  <button className="border-none py-1 px-3 md:py-2 md:px-4 text-base xl:text-lg capitalize">
-                    Login
-                  </button>
-                </Link>
-              </li>
-              <li className="hover:font-bold w-full py-5">
-                <Link href="/sign-up" onClick={() => setOpen(!open)}>
-                  <button className="text-black capitalize bg-white py-1 px-3 md:py-2 md:px-4 text-base xl:text-xl">
-                    try paystride for free
-                  </button>
-                </Link>
-              </li>
-            </ul>
-            <Image src={Cmark} alt="Vector" className="absolute top-0 left-0" />
-            <Image
-              src={TopMark}
-              alt="Vector"
-              className="absolute top-20 left-[55%] z-10"
-            />
-            <Image
-              src={Wavy}
-              alt="Vector"
-              className="absolute top-10 left-[10%] h-full w-[90%]"
-            />
-            <Image
-              src={BottomMark}
-              alt="Vector"
-              className="absolute bottom-10 left-0"
-            />
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
